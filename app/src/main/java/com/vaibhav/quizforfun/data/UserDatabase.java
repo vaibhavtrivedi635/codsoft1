90814ddd84d0f42f1e5c77b3b9411996ad810965
vaibhav <vaibhavtrivedi117@gmail.com>
2024-07-16 16:25:24 +0530

    initial commit

---
+++ app/src/main/java/com/vaibhav/quizforfun/data/UserDatabase.java
@@ -0,0 +1,15 @@
+package com.vaibhav.quizforfun.data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(
+entities = {User.class,Attempt.class},
+        exportSchema = false,
+        version = 1
+)
+public abstract class UserDatabase extends RoomDatabase {
+
+    public abstract UserDao userDao();
+
+}
